3e6f1914ccfa28a2925bfda14e814fe1eff43127
Sharmila Vijayakumar <sharmivijay94@gmail.com>
2025-08-05 09:52:19 +0530

    bg image height update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import { Expenses } from './expenses';
 
 function App() {
   return (
-    <div className="app flex justify-center flex-col" style= {{ backgroundImage : `url("/3.png")`}}>
+    
+    <div className="app flex justify-center flex-col w-full min-h-screen" style= {{ backgroundImage : `url("/3.png")`}}>
        <div className='font-bold text-4xl flex justify-center mx-auto p-6'> Budget Planner </div>
       <div className='flex justify-center flex-col w-2/3 mx-auto border-purple-400 border-4 rounded-lg p-6'>
         {/* <Bears /> */}
